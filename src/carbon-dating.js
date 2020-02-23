772a0333ee1334e7843fac64430a6af8113609b0
Baranov Igor <igor.82bv@mail.ru>
2020-02-23 22:33:17 +0300

    feat: solution carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let activity = +sampleActivity;
+  return typeof sampleActivity != 'string' ||
+    sampleActivity <= 0 ||
+    isNaN(activity) ||
+    sampleActivity > MODERN_ACTIVITY
+    ? false
+    : Math.ceil(
+        Math.log(MODERN_ACTIVITY / activity) / (0.693 / HALF_LIFE_PERIOD)
+      );
 };
