e01eb65ed29aadd09c766be79a3b4d67d9ba88f8
Baranov Igor <igor.82bv@mail.ru>
2020-02-24 17:11:48 +0300

    feat: solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let newlyTeam = [];
+  if (!Array.isArray(members)) {
+    return false;
+  } else {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === 'string') {
+        newlyTeam.push(
+          members[i]
+            .trim()
+            .toUpperCase()
+            .slice(0, 1)
+        );
+      }
+    }
+    return newlyTeam.sort().join('');
+  }
 };
